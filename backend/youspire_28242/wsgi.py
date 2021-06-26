5496cea80fd3f0985d02c80d67780805a45f7ada
Crowdbotics <team@crowdbotics.com>
2021-06-26 12:45:43 +0000

    Update models

--- backend/youspire_28242/wsgi.py
+++ backend/youspire_28242/wsgi.py
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'youspire_28242.settings')
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "youspire_28242.settings")
 
 application = get_wsgi_application()
